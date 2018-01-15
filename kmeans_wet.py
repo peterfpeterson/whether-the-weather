08302d344b13a0dff881468595195a2178be6cd3
Pete Peterson <Pete Peterson>
2018-01-14 21:17:18 -0500

    Checkpoint

--- kmeans_wet.py
+++ kmeans_wet.py
@@ -4,22 +4,24 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.cluster import KMeans
+from month_marks import month_ticks, month_label, colors
 
 # load the data
 print('loading TYS_daily.h5')
 dataframe = pd.read_hdf('TYS_daily.h5', 'everything').dropna()
 print('total rows', len(dataframe))
+dataframe.willrain = dataframe.willrain.astype(int)
 
 # split into train and test
-training = dataframe[dataframe.index < np.datetime64('2017-12-01')]
+training = dataframe[(dataframe.index >= np.datetime64('1997-01-01')) & (dataframe.index < np.datetime64('2017-12-01'))]
 test = dataframe[dataframe.index >= np.datetime64('2017-12-01')]
 
-labels_class = [#'wind_speed',
-                #'temp_min',
-                #'temp_max',
-                #'dew_min',
-                #'dew_max',
-                #'pressure',
+labels_class = ['wind_speed',
+                'temp_min',
+                'temp_max',
+                'dew_min',
+                'dew_max',
+                'pressure',
                 'press_change',
                 'precip1',
                 #'snow',
@@ -27,19 +29,22 @@ labels_class = [#'wind_speed',
                 ]
 
 # run it
-decision = KMeans(n_clusters=3)
+decision = KMeans(n_clusters=4)
 decision.fit(training[labels_class],
              training[['willrain']])#,'willsnow']])
 print(decision.score(test[labels_class],
                test[['willrain']]))#,'willsnow']]))
+print(np.unique(decision.labels_))
 prediction = decision.predict(test[labels_class])
 
 fig, ax = plt.subplots()
-days = np.arange(len(test))+1.
-ax.plot(days, test.precip1.values, label='rain')
-#ax.plot(days, test.snow.values, label='snow')
-ax.plot(days, prediction, label='pred')
-#ax.plot(days, prediction[1])
+days = np.arange(len(test))+365.-31
+ax.plot(days, test.precip1.values, label='rain', color=colors[6])
+ax.plot(days, prediction, label='prediction', color=colors[0])
 ax.legend()
-ax.set_xlim(1,32)
+ax.set_title('K-means clustering (4 clusters)')
+ax.set_xticks(month_ticks)
+ax.set_xticklabels(month_label)
+ax.set_ylabel('rain in cm')
+ax.set_xlim(334,364.5)
 fig.show()
--- tree_wet.py
+++ tree_wet.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn import tree
+from month_marks import month_ticks, month_label, colors
 
 # load the data
 print('loading TYS_daily.h5')
@@ -11,7 +12,7 @@ dataframe = pd.read_hdf('TYS_daily.h5', 'everything').dropna()
 print('total rows', len(dataframe))
 
 # split into train and test
-training = dataframe[dataframe.index < np.datetime64('2017-12-01')]
+training = dataframe[(dataframe.index >= np.datetime64('1997-01-01')) & (dataframe.index < np.datetime64('2017-12-01'))]
 test = dataframe[dataframe.index >= np.datetime64('2017-12-01')]
 
 labels_class = [#'wind_speed',
@@ -35,11 +36,15 @@ print(decision.score(test[labels_class],
 prediction = decision.predict(test[labels_class]).transpose()
 
 fig, ax = plt.subplots()
-days = np.arange(len(test))+1.
-ax.plot(days, test.precip1.values, label='rain')
+days = np.arange(len(test))+365.-31
+ax.plot(days, test.precip1.values, label='rain', color=colors[6])
 #ax.plot(days, test.snow.values, label='snow')
-ax.step(days, prediction[0], where='mid', label='pred')
+ax.step(days, prediction[0], where='mid', label='prediction', color=colors[0])
 #ax.plot(days, prediction[1])
 ax.legend()
-ax.set_xlim(1,32)
+ax.set_title('Decision Tree')
+ax.set_xticks(month_ticks)
+ax.set_xticklabels(month_label)
+ax.set_ylabel('rain in cm')
+ax.set_xlim(334,364.5)
 fig.show()
